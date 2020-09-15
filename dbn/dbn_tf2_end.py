f466d25de0e74b2412517e1f7868bb668b219fdb
jirufengyu <1037417707@qq.com>
2020-09-15 18:45:46 +0800

    master

--- dbn/dbn_tf2_end.py
+++ dbn/dbn_tf2_end.py
@@ -21,24 +21,12 @@ class RBM(object):
         return tf.math.sigmoid(tf.linalg.matmul(hidden, tf.transpose(w)) + vb)
     def sample_prob(self,probs):
         return tf.nn.relu(tf.sign(probs-tf.random.uniform(tf.shape(probs))))
-    def train(self,X,epochs=2,batchsize=128):
-        _w=tf1.placeholder("float",[self._input_size,self._output_size])
-        _hb=tf1.placeholder("float",[self._output_size])
-        _vb=tf1.placeholder("float",[self._input_size])
-
-        prv_w=np.zeros([self._input_size,self._output_size],np.float32)
-        prv_hb=np.zeros([self._output_size],np.float32)
-        prv_vb=np.zeros([self._input_size],np.float32)
-
-        cur_w=np.zeros([self._input_size,self._output_size],np.float32)
-        cur_hb=np.zeros([self._output_size],np.float32)
-        cur_vb=np.zeros([self._input_size],np.float32)
-        v0=tf1.placeholder("float",[None,self._input_size])
-
+    @tf.function
+    def runG(batch,_w,_hb,_vb):
+        v0=batch
         h0=self.sample_prob(self.prob_h_given_v(v0,_w,_hb))
         v1=self.sample_prob(self.prob_v_given_h(h0,_w,_vb))
         h1=self.prob_h_given_v(v1,_w,_hb)
-
         positive_grad=tf.linalg.matmul(tf.transpose(v0),h0)
         negative_grad=tf.linalg.matmul(tf.transpose(v1),h1)
 
@@ -47,26 +35,29 @@ class RBM(object):
         update_hb=_hb+self.learning_rate*tf.reduce_mean(h0-h1,0)
 
         err=tf.reduce_mean(tf.square(v0-v1))
+        return update_w,update_hb,update_vb
+    def train(self,X,epochs=2,batchsize=128):
+        prv_w=np.zeros([self._input_size,self._output_size],np.float32)
+        prv_hb=np.zeros([self._output_size],np.float32)
+        prv_vb=np.zeros([self._input_size],np.float32)
 
-        with tf1.Session() as sess:
-            sess.run(tf1.global_variables_initializer())
-            for epoch in range(epochs):
-                for start,end in zip(range(0,len(X),batchsize),range(batchsize,len(X),batchsize)):
-                    batch=X[start:end]
-                    cur_w=sess.run(update_w,feed_dict={v0:batch,_w:prv_w,_hb:prv_hb,_vb:prv_vb})
-                    cur_hb = sess.run(update_hb, feed_dict={v0: batch, _w: prv_w, _hb: prv_hb, _vb: prv_vb})
-                    cur_vb = sess.run(update_vb, feed_dict={v0: batch, _w: prv_w, _hb: prv_hb, _vb: prv_vb})
-                    prv_w=cur_w
-                    prv_hb=cur_hb
-                    prv_vb=cur_vb
-                error=sess.run(err,feed_dict={v0:X,_w: cur_w, _vb: cur_vb, _hb: cur_hb})
+        cur_w=np.zeros([self._input_size,self._output_size],np.float32)
+        cur_hb=np.zeros([self._output_size],np.float32)
+        cur_vb=np.zeros([self._input_size],np.float32)    
+        for epoch in range(epochs):
+            for start,end in zip(range(0,len(X),batchsize),range(batchsize,len(X),batchsize)):
+                batch=X[start:end]
+                cur_w,cur_hb,cur_vb=runG(batch,prv_w,prv_hb,prv_vb)
+                
+                prv_w=cur_w
+                prv_hb=cur_hb
+                prv_vb=cur_vb
+            error=runG(X,cur_w,cur_vb,cur_hb)        
+            print('Epoch: %d' % epoch, 'reconstruction error: %f' % error)
 
-
-                print('Epoch: %d' % epoch, 'reconstruction error: %f' % error)
-
-            self.w=prv_w
-            self.hb=prv_hb
-            self.vb=prv_vb
+        self.w=prv_w
+        self.hb=prv_hb
+        self.vb=prv_vb
     
     def rbm_outpt(self, X):
         input_X = tf.constant(X,dtype=tf.float32)
