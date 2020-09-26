31387c480c38482f79cf331ceb511d1e5aed83e0
jirufengyu <1037417707@qq.com>
2020-09-26 11:30:10 +0000

    master

--- dbn/rbm_revised_tf1.py
+++ dbn/rbm_revised_tf1.py
@@ -36,6 +36,21 @@ class RBM(object):
 
         loss=tf.reduce_mean(tf.square(v0-v1))
         return update_w,update_hb,update_vb,loss
+    def reverse_runG(self,h,_w,_hb,_vb):
+        h0=tf.cast(h,tf.float32)
+
+        v0=self.sample_prob(self.prob_v_given_h(h0,_w,_vb))
+        h1=self.sample_prob(self.prob_h_given_v(v0,_w,_vb))
+        v1=self.prob_v_given_h(h1,_w,_hb)
+
+        positive_grad=tf.matmul(tf.transpose(h0),v0)
+        negative_grad=tf.matmul(tf.transpose(h1),v1)
+
+        update_w=_w+self.learning_rate*(positive_grad-negative_grad)/tf.to_float(tf.shape(h0)[0])
+        update_hb=_hb+self.learning_rate*tf.reduce_mean(h0-h1,0)
+        update_vb=_vb+self.learning_rate*tf.reduce_mean(v0-v1,0)
+        loss=tf.reduce_mean(tf.square(h0-h1))
+        return update_w,update_hb,update_vb,loss
     def train(self,X,epochs=5,batchsize=100):
         prv_w=self.w
         prv_hb=self.hb
@@ -51,25 +66,57 @@ class RBM(object):
         #cur_hb=np.zeros([self._output_size],np.float32)
         #cur_vb=np.zeros([self._input_size],np.float32)    
         with tf.Session() as sess:
-            for epoch in range(epochs):
-                for start,end in zip(range(0,len(X),batchsize),range(batchsize,len(X),batchsize)):
-                    batch=X[start:end]
-                    cur_w,cur_hb,cur_vb,_=sess.run([cur_wt,cur_hbt,cur_vbt,_t], feed_dict={v0: batch, _w: prv_w, _hb: prv_hb, _vb: prv_vb})
-                    #cur_w,cur_hb,cur_vb,_=self.runG(batch,prv_w,prv_hb,prv_vb)
-                    
-                    prv_w=cur_w
-                    prv_hb=cur_hb
-                    prv_vb=cur_vb
-                    print("!!!!!!!!!!",prv_w)
+            #for epoch in range(epochs):
+                #for start,end in zip(range(0,len(X),batchsize),range(batchsize,len(X),batchsize)):
+            batch=X#[start:end]
+            cur_w,cur_hb,cur_vb,_=sess.run([cur_wt,cur_hbt,cur_vbt,_t], feed_dict={v0: batch, _w: prv_w, _hb: prv_hb, _vb: prv_vb})
+            #cur_w,cur_hb,cur_vb,_=self.runG(batch,prv_w,prv_hb,prv_vb)
+            
+            prv_w=cur_w
+            prv_hb=cur_hb
+            prv_vb=cur_vb
+            #print("!!!!!!!!!!",prv_w)
                    
-                _a,_b,_c,loss=self.runG(X,cur_w,cur_hb,cur_vb)        
-                print('Epoch: %d' % epoch)#, 'loss: %f' % loss[-1])
+                #_a,_b,_c,loss=self.runG(X,cur_w,cur_hb,cur_vb)        
+                #print('Epoch: %d' % epoch)#, 'loss: %f' % loss[-1])
 
         self.w=prv_w
         self.hb=prv_hb
         self.vb=prv_vb
-    
-    def rbm_outpt(self, X):
+    def reverse_train(self,X,epochs=5,batchsize=100):
+        prv_w=self.w
+        prv_hb=self.hb
+        prv_vb=self.vb
+        
+        v0= tf.placeholder("float", [None, self._output_size])
+        _w = tf.placeholder("float", [self._input_size, self._output_size])
+        _hb = tf.placeholder("float", [self._output_size])
+        _vb = tf.placeholder("float", [self._input_size])
+        cur_wt,cur_hbt,cur_vbt,_t=self.runG(v0,_w,_hb,_vb)
+
+        #cur_w=np.zeros([self._input_size,self._output_size],np.float32)
+        #cur_hb=np.zeros([self._output_size],np.float32)
+        #cur_vb=np.zeros([self._input_size],np.float32)    
+        with tf.Session() as sess:
+            #for epoch in range(epochs):
+                #for start,end in zip(range(0,len(X),batchsize),range(batchsize,len(X),batchsize)):
+            batch=X#[start:end]
+            cur_w,cur_hb,cur_vb,_=sess.run([cur_wt,cur_hbt,cur_vbt,_t], feed_dict={v0: batch, _w: prv_w, _hb: prv_hb, _vb: prv_vb})
+            #cur_w,cur_hb,cur_vb,_=self.runG(batch,prv_w,prv_hb,prv_vb)
+            
+            prv_w=cur_w
+            prv_hb=cur_hb
+            prv_vb=cur_vb
+            #print("!!!!!!!!!!",prv_w)
+                   
+                #_a,_b,_c,loss=self.runG(X,cur_w,cur_hb,cur_vb)        
+                #print('Epoch: %d' % epoch)#, 'loss: %f' % loss[-1])
+
+        self.w=prv_w
+        self.hb=prv_hb
+        self.vb=prv_vb
+
+    def rbm_output(self, X):
         input_X = tf.constant(X)
         _w = tf.constant(self.w)
         _hb = tf.constant(self.hb)
@@ -77,10 +124,20 @@ class RBM(object):
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             return sess.run(out)
+    
+    def rbm_output_reverse(self,X):
+        input_X = tf.constant(X)
+        _w = tf.constant(self.w)
+        _vb = tf.constant(self.vb)
+        out = tf.nn.sigmoid(tf.matmul(input_X, _w) + _vb)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            return sess.run(out)
 
 
-mnist = input_data.read_data_sets("MNIST_data/")
-trX, trY = mnist.train.images, mnist.train.labels
+#mnist = input_data.read_data_sets("MNIST_data/")
+#trX, trY = mnist.train.images, mnist.train.labels
+trX=np.array(500,784)
 rbm=RBM(784,500)
 
-rbm.train(trX)
+rbm.reverse_train(trX)
