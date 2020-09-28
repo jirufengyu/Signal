5b40cefa39992a44c5a5f953eb488b5c89cff5dc
swc_20200029 <43691125+jirufengyu@users.noreply.github.com>
2020-09-28 19:33:07 +0800

    master

--- Deep-Spectral-Clustering-using-Dual-Autoencoder-Network-master/src/applications/DSCDAN.py
+++ Deep-Spectral-Clustering-using-Dual-Autoencoder-Network-master/src/applications/DSCDAN.py
@@ -34,27 +34,12 @@ def run_net(data, params):
         y_sp = x_val_y.argmax(axis=1)
         print_accuracy(y_sp, y_val, params['n_clusters'])
         print("Epoch: {}, loss={:2f}".format(i, losses_vae[i]))
-        # else:
-        #     losses_vae[i] = ConvAE.train_vae(x_val, x_val_y,params['batch_size'])
-        #     x_val_y = ConvAE.vae.predict(x_val)[2]
-        #     y_sp = x_val_y.argmax(axis=1)
-        #     print_accuracy(y_sp, y_val, params['n_clusters'])
-        #     print("Epoch: {}, loss={:2f}".format(i, losses_vae[i]))
-
 
         if i>1:
             if np.abs(losses_vae[i]-losses_vae[i-1])<0.0001:
                 print('STOPPING EARLY')
                 break
 
-        # if self.lh.on_epoch_end(i, val_losses[i]):
-        #     print('STOPPING EARLY')
-        # break
-    # print training status
-
-    # ConvAE.vae.save_weights('IJCAI_mnist2.h5')
-    # spectral_net.net.save_weight('save.h5')
-    # spectral_net.save
     print("finished training")
 
     x_val_y = ConvAE.vae.predict(x_val)[2]
@@ -66,11 +51,5 @@ def run_net(data, params):
     nmi_score1 = nmi(y_sp, y_val)
     print('NMI: ' + str(np.round(nmi_score1, 4)))
 
-    # tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
-    # Z_tsne = tsne.fit_transform(x_val_y)
-    # fig = plt.figure()
-    # plt.scatter(Z_tsne[:, 0], Z_tsne[:, 1], s=2, c=y_train_unlabeled, cmap=plt.cm.get_cmap("jet", 10))
-    # plt.colorbar(ticks=range(10))
-    # plt.show()
 
 
